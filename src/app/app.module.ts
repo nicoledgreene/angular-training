de582facde991b963220d2e61338b6c93286666f
nicoledgreene <nicolegreene@Nicoles-MBP.lan1>
2020-10-12 13:21:39 -0700

    Started Angular training - created components for home and restaurant

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RestaurantComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
